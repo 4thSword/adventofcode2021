6dcdba5d4eb0330800212f3f5ab10f373b7f0669
Jesus Rodriguez <jralvarez1986@gmail.com>
2021-12-12 00:31:20 +0100

    day1 part1

---
+++ day-01/part1/day01_1.py
@@ -0,0 +1,28 @@
+# measures
+with open('input_1.txt') as f:
+    lines = f.readlines()
+
+measures = []
+
+for line in lines:
+    measures.append(int(line))
+
+print(measures[:10])
+
+larger = 0
+smaller = 0
+equals = 0
+
+for i in range(1, len(measures)):
+    if measures[i] > measures[i-1]:
+        larger += 1
+    elif measures[i] < measures[i-1]:
+        smaller += 1
+    else:
+        equals +=1
+
+print(f'''
+Larger = {larger}
+Smaller = {smaller}
+Equals = {equals}
+''')
