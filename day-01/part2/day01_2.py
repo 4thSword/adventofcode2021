52d12f7cd50045c5c63dccb55788ec9165a971bc
Jesus Rodriguez <jralvarez1986@gmail.com>
2021-12-12 01:22:37 +0100

    day1 part2

---
+++ day-01/part2/day01_2.py
@@ -0,0 +1,31 @@
+with open('./input_1.txt') as f:
+    lines = f.readlines()
+
+measures = []
+sum_measures = []
+
+for line in lines:
+    measures.append(int(line))
+
+
+larger = 0
+smaller = 0
+equals = 0
+
+for i in range(len(measures)-2):
+    sum_measures.append(measures[i] + measures[i+1] + measures[i+2])
+
+
+for i in range(1, len(sum_measures)):
+    if sum_measures[i] > sum_measures[i-1]:
+        larger += 1
+    elif sum_measures[i] < sum_measures[i-1]:
+        smaller += 1
+    else:
+        equals +=1
+
+print(f'''
+Larger = {larger}
+Smaller = {smaller}
+Equals = {equals}
+''')
